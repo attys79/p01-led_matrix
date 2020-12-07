414bead3f7efd90412802f2375fb821fc1308e99
attys79 <75606949+attys79@users.noreply.github.com>
2020-12-07 10:49:40 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
 basic.forever(function () {
-	
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(200)
+    basic.clearScreen()
+    basic.pause(200)
 })
