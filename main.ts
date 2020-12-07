3ccc59fb0163e81613cd7ff1ff6e0806343602fd
attys79 <75606949+attys79@users.noreply.github.com>
2020-12-07 11:56:17 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+for (let y = 0; y <= 4; y++) {
+    for (let x = 0; x <= 4; x++) {
+        led.plot(x, y)
+        basic.pause(250)
+        led.unplot(x, y)
+        basic.pause(250)
+    }
+}
 basic.forever(function () {
 	
 })
