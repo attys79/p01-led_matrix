6e7eaa6d5b0fae05a43d60ac782db6c5a7a4917b
attys79 <75606949+attys79@users.noreply.github.com>
2020-12-07 12:05:34 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
+for (let i = 0; i <= 255; i++) {
+    led.setBrightness(i)
+    basic.pause(1)
+}
 basic.forever(function () {
 	
 })
